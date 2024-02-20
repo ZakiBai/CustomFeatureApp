7897e06a55dcdefb9e894a0ca745e1b4aa2e1874
Zaki <menza.zm@gmail.com>
2024-02-20 12:43:14 +0600

    Storyboard removed

--- CustomFeatureApp/ViewController.swift
+++ CustomFeatureApp/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 6
+        button.tintColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        addViews()
+        addConstraints()
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    func addViews() {
+        view.addSubview(button)
+    }
+    
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            button.widthAnchor.constraint(equalToConstant: 100)
+        ])
     }
 
-
+    @objc private func buttonTapped() {
+        print("Hello world")
+    }
 }
 
